1cabf44f95811f737c83c6447452a28698bf0438
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2023-12-16 17:29:24 +0100

    Add Sailor model, update SailorsComponent and SailorsService

--- src/app/sailors/sailors.component.ts
+++ src/app/sailors/sailors.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SailorsService } from './sailors.service';
+import { Sailor } from './models/sailor';
 
 @Component({
   selector: 'app-sailors',
@@ -11,4 +13,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './sailors.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SailorsComponent { }
+export class SailorsComponent implements OnInit{
+
+  sailors: Sailor[] = [];
+
+  constructor(private sailorsService: SailorsService) { }
+
+  ngOnInit(): void {
+    this.sailorsService.getSailors().subscribe(sailors => {
+      this.sailors = sailors;
+    });
+  }
+}
--- src/app/sailors/sailors.service.ts
+++ src/app/sailors/sailors.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Sailor } from './models/sailor';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,8 @@ export class SailorsService {
 
   constructor(private http: HttpClient) { }
 
-  getSailors() {
-    return this.http.get('http://localhost:3000/sailors');
+  getSailors(): Observable<Sailor[]> {
+    return this.http.get<Sailor[]>('http://localhost:8080/sailors');
   }
 
 }
